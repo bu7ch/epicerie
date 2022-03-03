fd99191d84b33d63ea25e696d80a78d53b0e0bee
Rodolphe <dutchr@live.fr>
2022-03-03 16:54:54 +0100

    :sparkles:ajout des constraintes sur la class utilisateur

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,23 +2,28 @@
 
 namespace App\Entity;
 
-use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UtilisateurRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[UniqueEntity(fields:"username", message:"le user existe déjà")]
 class Utilisateur
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
-
+    
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Length(min:5,max:10, minMessage:"Il faut plus de 5 caractères", maxMessage: "Il faut mains de 10 caractè")]
     private $username;
 
     #[ORM\Column(type: 'string', length: 255)]
     private $password;
-
+    
+    #[Assert\EqualTo(propertyPath:"password", message:"Les mdp ne sont pas equivalent")]
     private $verificationPassword;
 
     public function getId(): ?int
